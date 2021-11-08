75944f501f3b99fc0c892e1b5a2c05c26cb6d80e
shahmirghouri <shahmir.ghouri99@gmail.com>
2021-11-09 02:16:32 +0500

    react-Navigation-ini

--- App.js
+++ App.js
@@ -1,94 +1,131 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  * @flow strict-local
+//  */
 
-import React from 'react';
-import Home from './components/Home';
-//import type {Node} from 'react';
-import Login from './components/Login'
-import {
-  Button,
-  Text,
-  View,
-  StyleSheet,
-  TextInput
-} from 'react-native';
-
-class App extends React.Component {
-  constructor(){
-    super();
-    this.state={
-      name:"",
-      pass:"",
-      address:"",
-    }
-  }
-
-  submit()
-  {
-      // alert("Data Submitted");
-      console.warn("all values", this.state);
-  }
+// import React from 'react';
+// import Home from './components/Home';
+// //import type {Node} from 'react';
+// import Login from './components/Login'
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import {
+//   Button,
+//   Text,
+//   View,
+//   StyleSheet,
+//   TextInput
+// } from 'react-native';
 
 
-  render() {
-    return (
-      <View>
-        <TextInput placeholder="Enter Name" style={[styles.borders]} 
-          onChangeText={(text)=> {this.setState({name:text})}} >
 
-        </TextInput>
+// class App extends React.Component {
+//   constructor(){
+//     super();
+//     this.state={
+//       name:"",
+//       pass:"",
+//       address:"",
+//     }
+//   }
 
-        <TextInput placeholder="Enter Password" style={[styles.borders] } secureTextEntry={true}
-          onChangeText={(text)=> {this.setState({pass:text})}} >
+//   const Stack = createNativeStackNavigator();
 
-        </TextInput>
+//   submit()
+//   {
+//       // alert("Data Submitted");
+//       console.warn("all values", this.state);
+//   }
 
-        <TextInput placeholder="Enter Address" style={[styles.borders]} 
-          onChangeText={(text)=> {this.setState({address:text})}} >
 
-        </TextInput>
+//   render() {
+//     return (
+//       <NavigationContainer>
+//         <Stack.Navigator> 
+//       <View>
+//         <TextInput placeholder="Enter Name" style={[styles.borders]} 
+//           onChangeText={(text)=> {this.setState({name:text})}} >
 
-        <Button title="Submit" onPress={()=>{this.submit()}}></Button>
-      </View>
+//         </TextInput>
 
-    )
-  }
+//         <TextInput placeholder="Enter Password" style={[styles.borders] } secureTextEntry={true}
+//           onChangeText={(text)=> {this.setState({pass:text})}} >
+
+//         </TextInput>
+
+//         <TextInput placeholder="Enter Address" style={[styles.borders]} 
+//           onChangeText={(text)=> {this.setState({address:text})}} >
+
+//         </TextInput>
+
+//         <Button title="Submit" onPress={()=>{this.submit()}}></Button>
+//       </View>
+//       </NavigationContainer>
+
+//     )
+//   }
+// }
+
+
+// const styles = StyleSheet.create({
+//   // colors:{
+//   //   color:'maroon',
+//   //   backgroundColor:'lightpink'
+
+//   // },
+//   // fonts:{
+//   //   fontSize: 30,
+//   //   fontFamily:'ariel'
+    
+//   // },
+//   //  backColor:{
+//   //    backgroundColor:'#808080'
+//   //  },
+
+//   borders:{
+//     borderColor:'skyblue',
+//     borderWidth:2,
+//     padding:10,
+//     marginHorizontal:10,
+//     marginVertical:10
+
+
+
+//   }
+// })
+
+
+
+
+// export default App;
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+     
+    </View>
+  );
 }
 
+const Stack = createNativeStackNavigator();
 
-const styles = StyleSheet.create({
-  // colors:{
-  //   color:'maroon',
-  //   backgroundColor:'lightpink'
-
-  // },
-  // fonts:{
-  //   fontSize: 30,
-  //   fontFamily:'ariel'
-    
-  // },
-  //  backColor:{
-  //    backgroundColor:'#808080'
-  //  },
-
-  borders:{
-    borderColor:'skyblue',
-    borderWidth:2,
-    padding:10,
-    marginHorizontal:10,
-    marginVertical:10
-
-
-
-  }
-})
-
-
-
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
