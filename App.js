11e41d77c1d54e1eb50362c110ea74399f41c8a9
shahmirghouri <shahmir.ghouri99@gmail.com>
2021-11-09 00:34:41 +0500

    Basic_form

--- App.js
+++ App.js
@@ -28,54 +28,64 @@ class App extends React.Component {
     }
   }
 
-  chngeData ()
-  {
-    
-  }
   submit()
   {
-      alert("Data Submitted");
+      // alert("Data Submitted");
+      console.warn("all values", this.state);
   }
 
 
   render() {
     return (
       <View>
-        <TextInput placeholder="Enter Name" style={[]} 
+        <TextInput placeholder="Enter Name" style={[styles.borders]} 
           onChangeText={(text)=> {this.setState({name:text})}} >
 
         </TextInput>
 
-        <TextInput placeholder="Enter Password" style={[]} 
+        <TextInput placeholder="Enter Password" style={[styles.borders] } secureTextEntry={true}
           onChangeText={(text)=> {this.setState({pass:text})}} >
 
         </TextInput>
 
-        <TextInput placeholder="Enter Address" style={[]} 
+        <TextInput placeholder="Enter Address" style={[styles.borders]} 
           onChangeText={(text)=> {this.setState({address:text})}} >
 
         </TextInput>
 
+        <Button title="Submit" onPress={()=>{this.submit()}}></Button>
       </View>
+
     )
   }
 }
 
 
 const styles = StyleSheet.create({
-  colors:{
-    color:'maroon',
-    backgroundColor:'lightpink'
+  // colors:{
+  //   color:'maroon',
+  //   backgroundColor:'lightpink'
 
-  },
-  fonts:{
-    fontSize: 30,
-    fontFamily:'ariel'
+  // },
+  // fonts:{
+  //   fontSize: 30,
+  //   fontFamily:'ariel'
     
-  },
-   backColor:{
-     backgroundColor:'#808080'
-   }
+  // },
+  //  backColor:{
+  //    backgroundColor:'#808080'
+  //  },
+
+  borders:{
+    borderColor:'skyblue',
+    borderWidth:2,
+    padding:10,
+    marginHorizontal:10,
+    marginVertical:10
+
+
+
+  }
 })
 
 
