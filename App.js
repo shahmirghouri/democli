231e326394cee2ee2e8db94e78f60bcf8489ff2e
shahmirghouri <shahmir.ghouri99@gmail.com>
2021-11-14 20:56:47 +0500

    Navigation_ini

--- App.js
+++ App.js
@@ -101,30 +101,20 @@
 
 
 // export default App;
+// In App.js in a new project
 
 import * as React from 'react';
 import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function HomeScreen() {
+import Routes from './src/Navigation/Routes'
+
+
+
+const App = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-     
+    <View style={{flex:1}}>
+      <Routes />
     </View>
-  );
-}
-
-const Stack = createNativeStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
   );
 }
 
